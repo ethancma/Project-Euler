a4531b58afca43a8fcc2877a3a24e2dad76d041f
Ethan Ma <ethanmaisawesome@gmail.com>
2021-06-06 05:27:27 -0700

    q4

--- python/q004.py
+++ python/q004.py
@@ -7,10 +7,13 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 def isPalindrome(n):
     return str(n) == str(n)[::-1]
     
-
 def main():
-    L = 123456654321
-    print(isPalindrome(L))
+    max = 100*100
+    for i in range(100,1000):
+        for j in range(100,1000):
+            if (isPalindrome(i*j) and ((i*j) > max)):
+                max = i*j
+    print(max)
 
 if __name__=='__main__':
     main()
